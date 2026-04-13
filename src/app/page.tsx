c4de623195495c390c3949133203663900474e23
halu <haluz1389@gmail.com>
2026-04-14 00:56:00 +0900

    Disable prerender on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import InventoryApp from "@/app/components/inventory-app";
 import { getInventorySnapshot } from "../lib/store";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const snapshot = await getInventorySnapshot();
 

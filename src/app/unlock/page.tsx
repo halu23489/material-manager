04f1d825bfbb4be47d462e875c737f657b6aa4bb
halu <haluz1389@gmail.com>
2026-04-20 00:16:16 +0900

    Harden unlock route as fully dynamic with revalidate=0

--- src/app/unlock/page.tsx
+++ src/app/unlock/page.tsx
@@ -1,6 +1,7 @@
 import UnlockPageClient from "./unlock-page-client";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default function UnlockPage() {
   return <UnlockPageClient />;
